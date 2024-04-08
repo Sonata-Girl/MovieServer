c85dbac20ddc912513f03894188ced17f924298c
SonataGirl <Ino-sonata1988>
2024-04-08 20:14:09 +0300

    Добавила POST запрос

---
+++ Sources/App/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//
+//
+//  Created by Sonata Girl on 08.04.2024.
+//
+
+import Foundation
+import Vapor
+
+struct Movie: Content {
+    let title: String
+    let year: Int
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,43 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+
+    // movies/anygenre
+    // movies/kids
+    // movies/horror
+    // movies/action
+    app.get("movies", ":genre") { req async throws -> String in
+        guard let genre = req.parameters.get("genre") else {
+            throw Abort(.badRequest)
+        }
+        return "All movies of genre: \(String(describing: genre))"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("customers", ":customerId") { req async throws -> String in
+        guard let customerId = req.parameters.get("customerId", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "\(customerId)"
     }
+
+    // movies/action/2023
+    app.get("movies", ":genre", ":year") { req async throws -> String in
+        guard let genre = req.parameters.get("genre"),
+            let year = req.parameters.get("year")
+        else {
+            throw Abort(.badRequest)
+        }
+
+        return "All movies of genre: \(genre) for year \(year)"
+    }
+
+    app.get("movies") { req async in
+        [Movie(title: "Batman", year: 2023), Movie(title: "Spiderman", year: 2022)]
+    }
+
+    app.post("movies") { req async throws in
+        let movie = try req.content.decode(Movie.self)
+        return movie
+    }
+
 }
